239a94079443462c30192ce2aa03d4eb0da6be23
Ozair Khan <ozairkhantech@gmail.com>
2024-12-07 23:31:54 +0530

    api configuiration for getting movie

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.scss']
 })
-export class SliderComponent {
+export class SliderComponent implements OnInit {
+  constructor(private http: HttpClient){}
 
+  ngOnInit(){
+    this.http.get(
+      'https://api.themoviedb.org/3/movie/popular?api_key=8c247ea0b4b56ed2ff7d41c9a833aa77'
+    ).subscribe((data) => {
+      console.log("data is present here : "+data)
+    })
+  }
 }
